90144e662c7fe93ce2b4900f456ddbed69d3132b
flytreeleft <flytreeleft@crazydan.org>
2025-02-03 21:31:17 +0800

    Components: 新增草稿提醒组件 DraftWarning

---
+++ src/components/DraftWarning/i18n.js
@@ -0,0 +1,17 @@
+import { i18n, arg } from '@site/src/components/I18n';
+
+export default i18n()
+  //
+  .trans(['warning.title'])
+  .zh('草稿提醒')
+  .en('Draft Warning')
+  //
+  .trans(['warning.content'])
+  .zh('本文还在编辑整理中，后续可能会发生较大变动，请谨慎阅读！')
+  .en(
+    'This article is still being edited,' +
+      ' and significant changes may occur in the future.' +
+      ' Please proceed with caution when reading!'
+  )
+  //
+  .done();
