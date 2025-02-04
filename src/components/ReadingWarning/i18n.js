7a2bbf9e2fb1e69395a22601851417f97fb05037
flytreeleft <flytreeleft@crazydan.org>
2025-02-04 11:29:38 +0800

    Component: DraftWarning -> ReadingWarning

--- src/components/ReadingWarning/i18n.js
+++ src/components/ReadingWarning/i18n.js
@@ -3,8 +3,8 @@ import { i18n, arg } from '@site/src/components/I18n';
 export default i18n()
   //
   .trans(['warning.title'])
-  .zh('草稿提醒')
-  .en('Draft Warning')
+  .zh('阅读提醒')
+  .en('Reading Warning')
   //
   .trans(['warning.content'])
   .zh('本文还在编辑整理中，后续可能会发生较大变动，请谨慎阅读！')
