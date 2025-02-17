7e513caee1620c8c81cbf1e4dffa50a67736179b
flytreeleft <flytreeleft@crazydan.org>
2025-02-17 12:11:00 +0800

    更新样式

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -5,23 +5,63 @@ import styles from './styles.module.css';
 
 export function Table({ children, head }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          {head.map((name) => {
-            return <th>{name}</th>;
-          })}
-        </tr>
-      </thead>
+    <table className={styles.table}>
+      {head && (
+        <thead>
+          <tr>
+            {head.map((name, idx) => {
+              return <th key={idx}>{name}</th>;
+            })}
+          </tr>
+        </thead>
+      )}
       <tbody>{children}</tbody>
     </table>
   );
 }
 
 export function TRow({ children }) {
-  return <tr>{children}</tr>;
+  const desc = [];
+  const other = [];
+  [].concat(children).forEach((child) => {
+    if (child.props.mdxType === 'TDesc') {
+      desc.push(child);
+    } else {
+      other.push(child);
+    }
+  });
+
+  return (
+    <>
+      <tr>{other}</tr>
+      {desc.length > 0 && (
+        <tr className={clsx(styles.tableRowDesc)}>
+          <td colSpan={other.length} className={clsx(styles.tableCellDesc)}>
+            {desc}
+          </td>
+        </tr>
+      )}
+    </>
+  );
 }
 
-export function TCol({ children }) {
-  return <td className={clsx(styles.tableColumn)}>{children}</td>;
+export function TCol({ children, id }) {
+  return (
+    <td
+      id={id}
+      className={clsx(styles.tableColumn, id && styles.tableCellWithId)}
+    >
+      {id && (
+        <a
+          href={'#' + id}
+          className={clsx(styles.tableCellHashLink, 'hash-link')}
+        ></a>
+      )}
+      {typeof children === 'string' ? children.trim() : children}
+    </td>
+  );
+}
+
+export function TDesc({ children }) {
+  return <>{children}</>;
 }
